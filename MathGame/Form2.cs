54ffe6182090cd57b297c06238b4ec4b5595e981
ilyasBayram <ilyas.bayram@hotmail.com>
2022-04-09 17:08:02 +0300

    tool tip fixed

--- MathGame/Form2.cs
+++ MathGame/Form2.cs
@@ -17,6 +17,34 @@ namespace MathGame
             InitializeComponent();
         }
 
+        private void comboBoxTip()
+        {
+            ToolTip toolTip1 = new ToolTip();
+            toolTip1.AutoPopDelay = 0;
+            toolTip1.InitialDelay = 0;
+            toolTip1.ReshowDelay = 0;
+            toolTip1.ShowAlways = true;
+            if (this.comboOperation.Name == "comboOperation")
+            {
+                toolTip1.SetToolTip(this.comboOperation, "You can choose operation type.");
+            }
+            if (this.comboLevel.Name == "comboLevel")
+            {
+                toolTip1.SetToolTip(this.comboLevel, "You can choose difficultiy of the game.");
+            }
+            if (this.comboSpeed.Name == "comboSpeed")
+            {
+                toolTip1.SetToolTip(this.comboSpeed, "You can choose speed of the game. \n " +
+                "It determines how many seconds later new question will appear.  ");
+            }
+            if (this.comboTime.Name == "comboTime")
+            {
+                toolTip1.SetToolTip(this.comboTime, "You can choose total time of the game.");
+            }
+
+
+        }
+
         private void Form2_Load(object sender, EventArgs e)
         {
             comboOperation.SelectedIndex = 0;
@@ -27,43 +55,22 @@ namespace MathGame
 
         private void comboOperation_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ToolTip toolTip1 = new ToolTip();
-            toolTip1.AutoPopDelay = 0;
-            toolTip1.InitialDelay = 0;
-            toolTip1.ReshowDelay = 0;
-            toolTip1.ShowAlways = true;
-            toolTip1.SetToolTip(this.comboOperation, "You can choose operation type.");
+            comboBoxTip();
         }
 
         private void comboLevel_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ToolTip toolTip1 = new ToolTip();
-            toolTip1.AutoPopDelay = 0;
-            toolTip1.InitialDelay = 0;
-            toolTip1.ReshowDelay = 0;
-            toolTip1.ShowAlways = true;
-            toolTip1.SetToolTip(this.comboLevel, "You can choose difficultiy of the game.");
+            comboBoxTip();
         }
 
         private void comboSpeed_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ToolTip toolTip1 = new ToolTip();
-            toolTip1.AutoPopDelay = 0;
-            toolTip1.InitialDelay = 0;
-            toolTip1.ReshowDelay = 0;
-            toolTip1.ShowAlways = true;
-            toolTip1.SetToolTip(this.comboSpeed, "You can choose speed of the game. \n " +
-                "It determines how many seconds later new question will appear.  ");
+            comboBoxTip();
         }
 
         private void comboTime_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ToolTip toolTip1 = new ToolTip();
-            toolTip1.AutoPopDelay = 0;
-            toolTip1.InitialDelay = 0;
-            toolTip1.ReshowDelay = 0;
-            toolTip1.ShowAlways = true;
-            toolTip1.SetToolTip(this.comboTime, "You can choose total time of the game.");
+            comboBoxTip();
         }
 
     }
